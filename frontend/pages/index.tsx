fca515207ed2eb0c0966a2bf63fa0c94677f8613
Inkyu Kim <inkyu0103@gmail.com>
2022-07-03 09:37:27 +0900

    chore: add eslint rule

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,10 @@
+import Item from "components/Item";
 import type { NextPage } from "next";
 
-const Home: NextPage = () => <div />;
+const Home: NextPage = () => (
+  <h1>
+    하이
+    <Item />
+  </h1>
+);
 export default Home;
